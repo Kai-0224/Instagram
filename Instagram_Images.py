356d0cebddab33952854d8588b7756b6497d1a0c
Kai <50993786+Kai-0224@users.noreply.github.com>
2025-08-22 15:39:49 +0800

    Update Instagram_Images.py

--- Instagram_Images.py
+++ Instagram_Images.py
@@ -124,13 +124,13 @@ def generate_images_with_analysis(analysis_result, en_content):
 # Generate images
 print("Attempting to generate images...")
 generated_images = generate_images_with_analysis(analysis, en_content)
+image_filename = f'imagen-product-image_{today}_{i}.jpg'
 
 if generated_images:
     for i, generated_image in enumerate(generated_images):
         if generated_image.text is not None:
             print(generated_image.text)
         elif generated_image.inline_data is not None:
-            image_filename = f'imagen-product-image_{today}_{i}.jpg'
             image = Image.open(BytesIO((generated_image.inline_data.data)))
             image.save('image_filename')
         print(f"Successfully generated and saved image: {image_filename}")
